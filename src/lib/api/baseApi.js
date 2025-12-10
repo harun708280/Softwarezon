9227aa9c7c78232b962b534899798709a950f7d1
Harun <harun708280@gmail.com>
2025-12-10 13:00:25 +0600

    update

--- src/lib/api/baseApi.js
+++ src/lib/api/baseApi.js
@@ -3,8 +3,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_URL, 
-   
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+    credentials: "include", // cookie সহ request
+    prepareHeaders: (headers) => {
+      headers.set("Content-Type", "application/json");
+      return headers;
+    },
   }),
   tagTypes: ["Subscribe"],
   endpoints: () => ({}),
